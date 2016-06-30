943b253bc39651d79154d2b51c7e7b4d635752a5
Abiezer Reyes <master21delakal@live.com>
2016-06-30 16:45:12 -0400

    first

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <meta charset="utf-8">
+    <script src="css/jquery.js"></script>
+    <script src="css/script.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <center class="content">
+        <div class="posts">
+            <img src="">
+        </div>
+    </center>
+
+    <div class="logo">
+        <h1><i> Prueba</i></h1>
+        <div class="row">
+
+        </div>
+    </div>
+
+    <div class="blob">
+        <center>
+            <a>+</a>
+        </center>
+    </div>
+</body>
+</html>
